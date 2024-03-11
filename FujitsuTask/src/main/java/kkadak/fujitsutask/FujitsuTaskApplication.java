ef80306e8947e5e442c1aa87347ca1fb249a2fbe
Karl Kadak <kadakkarl@gmail.com>
2024-03-11 12:27:30 +0200

    Created project

---
+++ FujitsuTask/src/main/java/kkadak/fujitsutask/FujitsuTaskApplication.java
@@ -0,0 +1,13 @@
+package kkadak.fujitsutask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsuTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujitsuTaskApplication.class, args);
+	}
+
+}
